3d6daf6153b6437c00922a1a3136192a54487a74
victor.malyavko <victor.malyavko@gmail.com>
2017-06-10 00:05:43 +0300

    http://jira.cherishdev.com:8080/browse/FLAT-2 has been implemented

--- alert.js
+++ alert.js
@@ -9,15 +9,42 @@ function timeout(selector) {
     }, 1000);
 }
 
-var arr = [];
-for (var i = 0; i < countOfPages; i++) {
-    $("div[class='col-md-6 col-lg-6 col-sm-12 col-xs-12 ng-scope'] .flat-container>a").each(function (index, value) {
-        arr.push($(value).attr("href"));
-    });
-    $("li[title='Next Page']>a")[0].click();
-    timeout("div[ng-hide='searchInProgress']:not(.ng-hide)");
+function collectAllFlatHrefs() {
+    var arr = [];
+    for (var i = 0; i < countOfPages; i++) {
+        $("div[class='col-md-6 col-lg-6 col-sm-12 col-xs-12 ng-scope'] .flat-container>a").each(function (index, value) {
+            arr.push($(value).attr("href"));
+        });
+        $("li[title='Next Page']>a")[0].click();
+        timeout("div[ng-hide='searchInProgress']:not(.ng-hide)");
+    }
+
+    return arr;
 }
 
+
+function z(index) {
+    if (index === countOfFlats) {
+        alert(correctIds);
+        return;
+    }
+
+    var myWindow = window.open('https://www.flat-club.com' + arr[index]);
+
+    myWindow.onload = function () {
+        var valueOfLastCalendarUpdate = $('#flatViewHostInfo div:not([class]) span', myWindow.document).parent().text().trim();
+
+        if (valueOfLastCalendarUpdate.includes('Today') || valueOfLastCalendarUpdate.includes('This week') || valueOfLastCalendarUpdate.includes('Yesterday')) {
+            correctIds.push(arr[index]);//add regex !
+        }
+
+        myWindow.close();
+
+        z(index + 1);
+    };
+}
+
+var arr = collectAllFlatHrefs();
 var correctIds = [];
 var countOfFlats;
 
@@ -25,28 +52,4 @@ if ((parseInt(myVar) == 0) || (parseInt(myVar) > arr.length))
     countOfFlats = arr.length;
 else
     countOfFlats = parseInt(myVar);
-
-
-for (var i = 0; i < countOfFlats; i++) {
-    myWindow = window.open('https://www.flat-club.com' + arr[i]);
-
-    var valueOfLastCalendarUpdate;
-
-    myWindow.onload = function () {
-        valueOfLastCalendarUpdate = $('#flatViewHostInfo div:not([class]) span', myWindow.document).parent().text().trim();
-    }
-
-
-    if (valueOfLastCalendarUpdate.includes('Today') || valueOfLastCalendarUpdate.includes('This week') || valueOfLastCalendarUpdate.includes('Yesterday')) {
-        correctIds.push(arr[i] + valueOfLastCalendarUpdate);//add regex !
-    }
-    myWindow.close();
-}
-
-alert(correctIds);
-
-// $("div[class='col-md-6 col-lg-6 col-sm-12 col-xs-12 ng-scope'] .flat-container>a").each(function (index, value) {
-//     console.log($(value).attr("href"))
-// });
-// $("li[title='Next Page']>a")[0].click();
-// $("div[ng-hide='searchInProgress']:not(.ng-hide)")
+z(0);
